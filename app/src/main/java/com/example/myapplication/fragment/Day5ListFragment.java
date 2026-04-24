d46ad6b7ff5b7a3ca3cc1ec33a4236e715917cb1
Fiona <fionalynfu@gmail.com>
2026-04-24 14:05:34 +0800

    remove not used import

--- app/src/main/java/com/example/myapplication/fragment/Day5ListFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/Day5ListFragment.java
@@ -10,31 +10,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.example.myapplication.R;
 import com.example.myapplication.adapter.Day5ListAdapter;
-import com.example.myapplication.adapter.ListAdapter;
 import com.example.myapplication.databinding.Day5ListFragmentBinding;
-import com.example.myapplication.model.GankArticle;
 import com.example.myapplication.model.ListViewModel;
 import java.util.List;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import com.example.myapplication.R;
-import com.example.myapplication.adapter.Day5ListAdapter;
-import com.example.myapplication.databinding.Day5ListFragmentBinding;
 import com.example.myapplication.model.Post;
-import java.util.List;
 
 public class Day5ListFragment extends Fragment {
 
